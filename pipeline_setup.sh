0759e2ec41cd9ccb12d9c55cd6df291b5262130e
Tay Shen <tshen@redhat.com>
2022-02-14 10:53:35 -0500

    working on scripting to automate setup

---
+++ pipeline_setup.sh
@@ -0,0 +1,29 @@
+OCHOST=$1
+
+if ! [[ -n "${OCHOST}" ]]; then
+  echo "Set openshift host as first param"
+  exit
+fi
+
+echo "${OCHOST}"
+LOGGEDIN=$(oc project | grep "$OCHOST" >/dev/null; echo $?)
+if [[ "$LOGGEDIN" == "0" ]]; then
+    echo "Sweet.  Already logged in."
+else
+    echo "Either not logged in or logged into the wrong ocp cluster"
+    exit 1
+fi
+
+MYPROJ=$(oc projects | grep tay-test >/dev/null; echo $?)
+if [[ "$MYPROJ" == "0" ]]; then
+    echo "tay-test already exists"
+    oc project tay-test
+else
+    oc new-project tay-test
+fi
+
+oc apply -f operators/pipelines.yml
+oc apply -f storage
+oc apply -f pipelines
+oc apply -f pipelines/tasks
+
